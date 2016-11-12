da0c5c5bb179bb45cc68994ce09742f404f50dd7
lorenzo <lorenzo@lore>
2016-11-12 12:44:19 +0100

    fix-travisbuild-problems

--- travis_build.sh
+++ travis_build.sh
@@ -3,7 +3,7 @@
 set -ev
    if [ "$WITH_COVERALLS" == "true" ];
       then
-         mvn -f com.examples.myproject/pom.xml clean verify -Pjacoco coveralls:report;
+         mvn -f com.examples.MyProject/pom.xml clean verify -Pjacoco coveralls:report;
       elif [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-         mvn -f com.examples.myproject/pom.xml clean verify;
+         mvn -f com.examples.MyProject/pom.xml clean verify;
    fi
